1d419f55c0e9d8ab72135c779e111ada1af0e25e
root <n.ntare@alustudent.com>
2024-11-16 03:11:33 -0800

    add files

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,17 @@
--- show all privileges of data users 
+-- Create user_0d_1 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Create user_0d_2 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant specific privileges to user_0d_2
+GRANT SELECT, INSERT, UPDATE ON *.* TO 'user_0d_2'@'localhost';
+
+-- List all privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List all privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
