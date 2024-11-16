10da0cf7d6ee84c3e1de48327a2380721f95bfd6
root <n.ntare@alustudent.com>
2024-11-16 03:17:06 -0800

    add files

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,17 +1,4 @@
--- Create user_0d_1 if it does not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user_0d_2 if it does not exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant specific privileges to user_0d_2
-GRANT SELECT, INSERT, UPDATE ON *.* TO 'user_0d_2'@'localhost';
-
--- List all privileges for user_0d_1
+-- lists privileges of the MySQL users
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List all privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
