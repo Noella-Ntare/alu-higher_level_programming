27528e0d4ad2e32020990fbd5085028bbc94744d
root <n.ntare@alustudent.com>
2024-11-18 02:35:10 -0800

    add files

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-'''a Python script that fetches a url'''
-
+'''A Python script that fetches a URL'''
 
 import urllib.request
 
-
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    url = 'https://intranet.hbtn.io/status'
+    # Create a Request object with a custom User-Agent header
+    request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    with urllib.request.urlopen(request) as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode("utf-8")))
+
