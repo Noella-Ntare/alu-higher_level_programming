71ac1cd19e075c7a10790e0e56ff45a389fdfa5b
root <n.ntare@alustudent.com>
2024-11-18 03:03:49 -0800

    add files

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-'''A Python script that fetches https://alu-intranet.hbtn.io/status'''
+"""Documented"""
+
 
 import urllib.request
 
+
 if __name__ == '__main__':
-    url = 'https://0.0.0.0:5050/status'
-
-    # Make the request
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
-
-    # Display the required output
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
