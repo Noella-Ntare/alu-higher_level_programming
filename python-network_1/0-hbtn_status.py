2cda59b92754413b59abb477efc9f45ed7367c3e
root <n.ntare@alustudent.com>
2024-11-18 02:15:40 -0800

    add files

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
-'''a Python script that fetches a url'''
-
-
+        #!/usr/bin/python3
 import urllib.request
 
+url = "https://alu-intranet.hbtn.io/status"
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+# Fetching the URL content
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+# Printing the response body details
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
