e3268f18b2425b46ede2672ed7f67a150ffa7497
root <n.ntare@alustudent.com>
2024-11-21 04:41:01 -0800

    add files

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Fetch a url"""
+"""Python script that fetchs."""
+
 
 import requests
-
-
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
+    requ = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(requ.text)))
+    print("\t- content: {}".format(requ.text))
