59396f79c687ebfa3e7492b225dc4e37c0a27d0b
unknown <n.ntare@alustudent.com>
2025-01-28 15:07:50 +0200

    arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,8 +4,8 @@ constant args = process.argv.slice(2);
 
 if (args.length === 0) {
     console.log('No argument');
-} else if (arg.length ===1) {
-    console.group('Argument found');
+} else if (arg.length === 1) {
+    console.log('Argument found');
 } else {
     console.log('Argument found');
 }
